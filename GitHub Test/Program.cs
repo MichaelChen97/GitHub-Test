cc23cd06a019ca14bd30b6d1ddf703cf97ac932a
MichaelChen97 <michael16585@outlook.com>
2025-08-21 13:39:05 +1200

    Add project files.

---
+++ GitHub Test/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GitHub_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1.45 pm, Aug 21 2025");
+        }
+    }
+}
